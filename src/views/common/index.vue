f47384285bf8782c9ea500ee49613f7f0626b6ba
liuzj@andata.cn <liuzj@andata.cn>
2021-05-19 19:07:14 +0800

    提交代码

---
+++ src/views/common/index.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+export default {
+    created() {
+        var appID = 'wxd4270c7f24a6f345' 
+        var redirect = encodeURIComponent('http://qcred3.natappfree.cc/front/#/home')
+        var url_userinfo 	= 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appID+'&redirect_uri='+redirect+'&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
+        location.replace(url_userinfo);
+    },
+}
+</script>
+
